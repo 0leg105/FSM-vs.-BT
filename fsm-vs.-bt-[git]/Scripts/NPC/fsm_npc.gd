3ec8e99566020aa2f9fd4c93f810d2ca9696629a
Franke, Janik <j.franke@neogramm.de>
2025-01-09 15:27:25 +0100

    Fix state labels

--- fsm-vs.-bt-[git]/Scripts/NPC/fsm_npc.gd
+++ fsm-vs.-bt-[git]/Scripts/NPC/fsm_npc.gd
@@ -3,7 +3,7 @@ class_name FsmNpc
 
 @onready var fsm: FiniteStateMachine = $"Enemy Finite State Machine"
 @onready var state_label: Label = $CenterContainer/StateLabel
-@onready var hud: HUD = %Hud
+@onready var hud: HUD
 var debug_box: Node2D
 
 func _on_state_changed(new_state):
@@ -12,13 +12,11 @@ func _on_state_changed(new_state):
 func _enter_tree() -> void:
 	super._enter_tree()
 	debug_box = get_tree().get_first_node_in_group("DebugBox")
-	hud = get_tree().get_first_node_in_group("hud")
-	#state_label.visible = hud.show_state_label
+	hud = global_coordinator.hud
 	state_manager.state_changed.connect(_on_state_changed)
 
 func _ready() -> void:
-	#state_label.visible = hud.show_state_label
-	pass
+	state_label.visible = hud.show_state_label
 
 func _process(delta: float) -> void:
 	vision_component.update()
