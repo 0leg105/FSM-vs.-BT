d41abae7ee4a7cd16d26929b91f8648e1c2a0f2e
Franke, Janik <j.franke@neogramm.de>
2025-01-13 12:14:56 +0100

    Add fsm and bt selection to normal mode

--- fsm-vs.-bt-[git]/Scripts/UI/menu.gd
+++ fsm-vs.-bt-[git]/Scripts/UI/menu.gd
@@ -4,6 +4,8 @@ extends Node2D
 @onready var waitTimeInput = $NormalMode/SpawnWaitTimeInput
 @onready var errorLabel = $NormalMode/ErrorLabel
 @onready var errorTimer = $NormalMode/ErrorLabel/Timer
+var use_fsm = true
+var use_bt = false
 
 func _on_start_button_pressed() -> void:
 	var maxNPCs = maxNPCsInput.value
@@ -20,6 +22,10 @@ func _on_start_button_pressed() -> void:
 			var spawner = child as NpcSpawner
 			spawner.max_npc_count = maxNPCsInput.value
 			spawner.wait_time = waitTimeInput.value
+			if spawner.name == "FsmNpcSpawner" and !use_fsm:
+				spawner.max_npc_count = 0
+			if spawner.name == "BtNpcSpawner" and !use_bt:
+				spawner.max_npc_count = 0
 	
 	get_tree().root.call_deferred("remove_child", get_tree().current_scene)
 	get_tree().root.add_child(game)
@@ -47,3 +53,9 @@ func _on_second_start_button_pressed() -> void:
 
 func _on_error_timer_timeout() -> void:
 	errorLabel.visible = false
+
+func _on_fsm_check_button_toggled(toggled_on: bool) -> void:
+	use_fsm = toggled_on
+
+func _on_bt_check_button_toggled(toggled_on: bool) -> void:
+	use_bt = toggled_on
